c944650feb61d853f9347dbfb1e7e10cd2e1c69c
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-03 14:33:32 -0800

    sending index and show data

---
+++ views/index.jsx
@@ -0,0 +1,29 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+const bread = require('../models/bread')
+
+function Index ({breads, title})  {
+    return (
+      <Default title={title}>
+        <h2>Index Page</h2>
+        {/* This is a JSX COMMENT */}
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        <ul>
+            {
+                breads.map((bread, index) => {
+                    return (
+                    <li key={index}>
+                        <a href={`/bread/${index}`}>
+                            {bread.name}
+                        </a>
+                    </li>
+                    )
+                })
+            }
+        </ul>
+      </Default>
+    )
+}
+
+
+module.exports = Index
